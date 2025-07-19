0e3980d11a57418b0f0b91a2aa10e033d3871288
hyleungtommy <104375225+hyleungtommy@users.noreply.github.com>
2025-07-19 11:15:52 -0400

    add world map

---
+++ Assets/Scripts/Controller/SceneController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance;
+    [SerializeField] Animator SceneTransitAnimator;
+    public static SceneList.TileMap CurrentTileMap { get; private set; }
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TransitScene(string sceneName){
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    public void AnimateSceneTransit(SceneList.TileMap tileMap){
+        StartCoroutine(TransitWithAnimation(tileMap));
+    }
+
+    IEnumerator TransitWithAnimation(SceneList.TileMap tileMap){
+        SceneTransitAnimator.SetTrigger("End");
+        yield return new WaitForSeconds(1f);
+        CurrentTileMap = tileMap;
+        SceneManager.LoadSceneAsync(tileMap.ToString());
+        SceneTransitAnimator.SetTrigger("Start");
+    }
+}
